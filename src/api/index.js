64041de293f4473fdaab0336304677612f011a7d
Tousif Khan <tkhan7916@gmail.com>
2020-12-08 16:18:01 -0500

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+    try {
+        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url);
+
+        return {confirmed, recovered, deaths, lastUpdate};
+    } catch (error) {
+
+    }
+};
+
+export const fetchDailyData = async () => {
+    try {
+        const {data} = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+
+        return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+    } catch (error) {
+        return error;      
+    }
+};
